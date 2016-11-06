83b15b05a71aaa027e7f605113a8b98b24f54c5c
Dennis Iversen <dennis.iversen@gmail.com>
2016-11-06 11:21:27 +0100

    readme

--- test.php
+++ test.php
@@ -1,21 +1,11 @@
-# get-meta-tags
+<?php
 
-PHP composer package for getting title and meta-tags
-Get meta tags using curl and DOM
-
-## Install
-
-    composer require diversen/get-meta-tags
-
-## Usage: 
-
-~~~php
-
-// include_once "../../vendor/autoload.php";
+include_once "../autoload.php";
 use diversen\meta;
 
 $m = new meta();
 
+return;
 // Simple usage, get's title, description, and keywords by default
 $ary = $m->getMeta('https://github.com/diversen/get-meta-tags');
 print_r($ary);
@@ -23,6 +13,3 @@ print_r($ary);
 // With more params
 $ary = $m->getMeta('https://github.com/diversen/get-meta-tags', array ('description' ,'keywords'), $timeout = 10);
 print_r($ary);
-~~~
-
-License [MIT](https://opensource.org/licenses/MIT)
